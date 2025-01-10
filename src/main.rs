d205fea592dad2589e8070910d733c2a4b766862
natsuakane <pacupacuman@outlook.com>
2025-01-10 21:33:14 +0900

    search function implemented

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use bevy::{
     ui::widget::NodeImageMode,
     window::PrimaryWindow,
 };
-use bevy_simple_text_input::{TextInput, TextInputPlugin};
+use bevy_simple_text_input::{TextInput, TextInputPlugin, TextInputSubmitEvent, TextInputSystem};
 
 mod block;
 
@@ -17,6 +17,7 @@ fn main() {
         .add_systems(Startup, spawn_trash_area) //ゴミ箱エリアを追加
         .add_systems(Update, move_camera) // マウス操作を登録
         .add_systems(Update, show_menu) // ブロック配置
+        .add_systems(Update, menu_search.after(TextInputSystem)) // テキストインプットイベント
         .insert_resource(block::DragState::default()) // リソース追加
         .insert_resource(block::BlockList::default()) // ブロックのリストを追加
         .add_systems(Update, block::drag_system) // ドラッグできるようにする
@@ -158,6 +159,9 @@ fn move_camera(
 #[derive(Component)]
 struct Menu;
 
+#[derive(Component)]
+struct BlockItem;
+
 fn show_menu(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -192,49 +196,108 @@ fn show_menu(
                         left: Val::Px(screen_pos.x),
                         top: Val::Px(screen_pos.y),
                         overflow: Overflow::clip(),
-                        flex_direction: FlexDirection::Column,
-                        padding: UiRect::all(Val::Px(15.0)),
-                        row_gap: Val::Px(3.0),
-                        column_gap: Val::Px(3.0),
                         ..Default::default()
                     },
                     BackgroundColor(Color::srgba(0.2, 0.2, 0.2, 0.8)),
                     Menu,
                 ))
                 .with_children(|parent| {
+                    parent
+                        .spawn((
+                            Node {
+                                width: Val::Px(170.0),
+                                border: UiRect::all(Val::Px(5.0)),
+                                flex_direction: FlexDirection::Column,
+                                padding: UiRect::all(Val::Px(15.0)),
+                                row_gap: Val::Px(3.0),
+                                column_gap: Val::Px(3.0),
+                                ..Default::default()
+                            },
+                            BackgroundColor::from(Color::srgba(0.2, 0.2, 0.2, 0.9)),
+                            TextInput,
+                        ))
+                        .with_children(|parent| {
+                            for i in 0..block_list.items.len() {
+                                parent
+                                    .spawn((
+                                        Button,
+                                        Node {
+                                            width: Val::Px(170.0),
+                                            height: Val::Px(20.0),
+                                            border: UiRect::all(Val::Px(5.0)),
+                                            ..Default::default()
+                                        },
+                                        BlockItem,
+                                        BackgroundColor::from(Color::srgba(0.2, 0.2, 0.2, 0.9)),
+                                    ))
+                                    .with_child((
+                                        Text::new(block_list.items[i].text.clone()),
+                                        TextFont {
+                                            font: asset_server.load("fonts/FiraCode-Medium.ttf"),
+                                            font_size: 10.0,
+                                            ..default()
+                                        },
+                                        TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                                    ));
+                            }
+                        });
+                });
+        }
+    }
+}
+
+fn menu_search(
+    mut commands: Commands,
+    mut events: EventReader<TextInputSubmitEvent>,
+    menus: Query<(Entity), With<Menu>>,
+    block_list: Res<block::BlockList>,
+    children: Query<(Entity, &Parent), With<BlockItem>>,
+    asset_server: Res<AssetServer>,
+) {
+    for event in events.read() {
+        for (child_entity, parent) in children.iter() {
+            if parent.get() == event.entity {
+                println!("AAA");
+                commands.entity(child_entity).despawn_recursive();
+            }
+        }
+
+        for i in 0..block_list.items.len() {
+            println!(
+                "{},{},{}",
+                &(event.value),
+                &block_list.items[i].text.clone(),
+                (&block_list.items[i].text.clone()).contains(&(event.value))
+            );
+            if !(&block_list.items[i].text.clone()).contains(&(event.value)) {
+                continue;
+            }
+            commands
+                .entity(event.entity)
+                .with_children(|parent| {
                     parent.spawn((
+                        Button,
                         Node {
                             width: Val::Px(170.0),
+                            height: Val::Px(20.0),
                             border: UiRect::all(Val::Px(5.0)),
                             ..Default::default()
                         },
+                        BlockItem,
                         BackgroundColor::from(Color::srgba(0.2, 0.2, 0.2, 0.9)),
-                        TextInput,
                     ));
-
-                    for i in 0..block_list.items.len() {
-                        parent
-                            .spawn((
-                                Button,
-                                Node {
-                                    width: Val::Px(170.0),
-                                    height: Val::Px(20.0),
-                                    border: UiRect::all(Val::Px(5.0)),
-                                    ..Default::default()
-                                },
-                                BackgroundColor::from(Color::srgba(0.2, 0.2, 0.2, 0.9)),
-                            ))
-                            .with_child((
-                                Text::new(block_list.items[i].text.clone()),
-                                TextFont {
-                                    font: asset_server.load("fonts/FiraCode-Medium.ttf"),
-                                    font_size: 10.0,
-                                    ..default()
-                                },
-                                TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                            ));
-                    }
-                });
+                })
+                .with_child((
+                    Text::new(block_list.items[i].text.clone()),
+                    TextFont {
+                        font: asset_server.load("fonts/FiraCode-Medium.ttf"),
+                        font_size: 10.0,
+                        ..default()
+                    },
+                    TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                ));
         }
+
+        println!("{:?} submitted: {}", event.entity, event.value);
     }
 }
